d8fc544b5d8f05910d51b8c7b2e8b854aa4e796c
ledat0711 <leanhdat1994@gmail.com>
2025-11-21 23:12:07 +0700

    DAT: WIP — Fix prisma.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,35 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ['query', 'info', 'warn', 'error'],
+//   })
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+// export default prisma
+
+
+
+
+
+
+
+
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const globalForPrisma = global as unknown as { 
+    prisma: PrismaClient
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  })
+const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
